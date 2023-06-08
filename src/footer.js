d817a8b85449e749d3adffd3e38ceac7f3582d21
Najah Farah <najahfarah27@gmail.com>
2023-06-08 20:25:51 +0100

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
-const props = ["123 Fake Street, London, E1 4UD", 
+const address = ["123 Fake Street, London, E1 4UD", 
 "hello@fakehotel.com", "0123 456789"] // create an array with the data
 
 function Footer(){
-    const footerAddress = props.map((list)=>{
+    const footerAddress = address.map((list)=>{
     return (<div className='footer' key={list}>
         <ul>
           <li>{list}</li>
