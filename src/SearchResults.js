9ba95dbb16cafc8ec9d26405a08a5ff23c2edc95
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-06-12 21:31:13 +0100

    Adding ID column

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+<<<<<<< Updated upstream
 function SearchResults() {
   return (
     <table class="table">
@@ -16,6 +17,26 @@ function SearchResults() {
       </thead>
       <tbody>
         <tr id="1">
+=======
+function SearchResults({ results }) {
+  return (
+    <>
+      <table className="table table-hover table-white">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room ID</th>
+            <th scope="col">Check-in Date</th>
+            <th scope="col">Check-out Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <tr id="1">
+>>>>>>> Stashed changes
           <th scope="row">Mr</th>
           <td>John</td>
           <td>Doe</td>
@@ -59,9 +80,32 @@ function SearchResults() {
           <td>3</td>
           <td>2017-08-30</td>
           <td>2017-10-02</td>
+<<<<<<< Updated upstream
         </tr>
       </tbody>
     </table>
+=======
+        </tr> */}
+          {results.map((element) => {
+            return (
+              <>
+                <tr>
+                  <td>{element.id}</td>
+                  <td>{element.title}</td>
+                  <td>{element.firstName}</td>
+                  <td>{element.surname}</td>
+                  <td>{element.email}</td>
+                  <td>{element.roomId}</td>
+                  <td>{element.checkInDate}</td>
+                  <td>{element.checkOutDate}</td>
+                </tr>
+              </>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+>>>>>>> Stashed changes
   );
 }
 
