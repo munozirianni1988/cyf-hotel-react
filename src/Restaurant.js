c5dc42a066573bc17fda6c578d02c8be9b9b32e1
Melese Berehannu <melemeko12@gmail.com>
2023-06-17 12:27:24 +0100

    changed

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,25 +1,17 @@
 import React from "react";
 
 const Restaurant = () => {
-<<<<<<< Updated upstream
-  const pizzas = 0;
-=======
   //const pizzas = 0;
   function orderOne(){
     return setOrders(orders + 1)
   }
   const [orders, setOrders] = useState(0);
->>>>>>> Stashed changes
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-<<<<<<< Updated upstream
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
-=======
           Pizzas: {orders} <button onClick={orderOne}className="btn btn-primary">Add</button>
->>>>>>> Stashed changes
         </li>
       </ul>
     </div>
