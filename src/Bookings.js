4acab8d25a1dd32f70bf68aad20c1a75f2e258d7
christina-mifsud <christina.mifsud@gmail.com>
2023-06-20 20:41:06 +0100

    Christina Exercise 16

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,12 +1,19 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings,setBookings]= useState(FakeBookings)
- 
+  const [bookings, setBookings] = useState([]);
 
+  useEffect(() => {
+    console.log("some text only when the page first renders on the screen");
+
+    fetch("https://cyf-react.glitch.me")
+      .then((response) => response.json())
+      .then((data) => setBookings(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
@@ -16,8 +23,7 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-       
-         <SearchResults results={bookings} /> 
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
