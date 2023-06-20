5a8f115e3260bd97e92fabd30cca81152a883fdb
christina-mifsud <christina.mifsud@gmail.com>
2023-06-20 19:54:03 +0100

    Exercise 12

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,19 +1,12 @@
 import React, { useState } from "react";
-import RestaurantButton from "./RestaurantButton";
+import Order from "./Order";
 
 const Restaurant = () => {
-  //const pizzas = 0;
-  function orderOne() {
-    return setOrders(orders + 1);
-  }
-  const [orders, setOrders] = useState(0);
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {orders} <RestaurantButton setOrder={orderOne} />
-        </li>
+        <Order />
       </ul>
     </div>
   );
