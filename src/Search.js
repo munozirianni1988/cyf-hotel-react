f0fe5a6eb820b0534b53208c8a7d7b1091026a5b
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-06-21 20:17:47 +0100

    exercise 18 done

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Search = () => {
+const Search = (props) => {
   const [searchInput, setSearchInput] = useState("");
 
   function handleSearchInput(event) {
@@ -15,7 +15,13 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              props.search(searchInput);
+            }}
+            className="form-group search-box"
+          >
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
